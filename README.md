7851d3824fa18f5ea69a982f9f3af6530737cc25
afc163 <afc163@gmail.com>
2018-01-01 17:04:54 +0800

    Add demo for parcel, close ant-design/ant-design#8716

---
+++ README.md
@@ -0,0 +1,20 @@
+# Ant Design Demo for parcel
+
+```
+npm install
+npm start
+```
+
+or
+
+```
+yarn
+yarn start
+```
+
+Visit http://localhost:1234
+
+---
+
+- https://parceljs.org/
+- http://ant.design/
---
+++ index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from 'antd';
+
+ReactDOM.render(
+  <Button type="primary">Button</Button>
+, document.getElementById('mountNode'));
